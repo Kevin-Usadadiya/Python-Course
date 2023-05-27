66e952a4df108493569c649b360effdf85e5f91c
Kevin <kevinu774@gmail.com>
2023-05-27 21:52:32 +0530

    Day 6 Notes update

---
+++ Notes/Day 6/main.py
@@ -0,0 +1,24 @@
+# Functions:-
+
+# How to create a Function?
+
+def my_function():
+    print("Hello");
+    print("bye");
+
+# How to use this Function?
+# Ans : Simply call this function.
+
+my_function();
+
+# Identation:-
+# --> Indentation refers to the spaces at the beginning of a code line.
+
+# While Loops:-
+#     Syntax:-
+#         while something_is_true:
+                #Do something repeatedly.
+a = 0
+while a < 3:
+    print("This will print 3 Times.")
+    a+=1
